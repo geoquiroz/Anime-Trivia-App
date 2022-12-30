833485b755aa6d0e2d4c21b6509636aa0e197109
geoquiroz <geo>
2022-12-30 11:49:56 -0500

    Anime Trivia

---
+++ Anime-Trivia/Anime-Trivia/Anime_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_TriviaApp.swift
+//  Anime-Trivia
+//
+//  Created by Geovanny quiroz on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Anime_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
